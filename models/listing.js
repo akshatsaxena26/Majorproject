1af54eacb2576dca6aec9b206f9543b0149387e7
akshatsaxena26 <akshatsaxena023@gmail.com>
2025-06-27 11:05:06 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,40 @@
+// const mongoose =require("mongoose")
+// const Schema=mongoose.Schema;
+
+// const listingSchema = new Schema({
+//     title:{
+//         type:String,
+//         required:true,
+//     },
+//     description:String,
+//     image:{
+//         type:String,
+       
+//     },
+//     price:Number,
+//     location:String,
+//     counntry:String,
+
+// })
+// const listing = mongoose.model("listing",listingSchema);
+// module.exports = listing;
+
+
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  image: {
+    filename: String,
+    url: String
+  },
+  price: Number,
+  location: String,
+  country: String
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
+
